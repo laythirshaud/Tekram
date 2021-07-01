13d116a0a3d966e4983536fc0e545d6b669c9521
Nasser Sayeh <nasser.sayeh89@gmail.com>
2021-07-01 16:52:58 +0300

    Tekram Project

---
+++ TekramProject/src/main/java/com/example/tekramproject/TekramProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.tekramproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TekramProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TekramProjectApplication.class, args);
+	}
+
+}
