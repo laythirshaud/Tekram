e059fe4ba12373281897a4f1eb55f0ca604a3268
Nasser Sayeh <nasser.sayeh89@gmail.com>
2021-07-03 09:46:51 +0300

    the new project

--- TekramProject-1/src/main/java/com/example/tekramproject/TekramProject1Application.java
+++ TekramProject-1/src/main/java/com/example/tekramproject/TekramProject1Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TekramProjectApplication {
+public class TekramProject1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TekramProjectApplication.class, args);
+		SpringApplication.run(TekramProject1Application.class, args);
 	}
 
 }
